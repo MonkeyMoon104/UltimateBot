a5e28ef55251a36c278cf6f336fb134263b4b37b
MonkeyMoon104 <lollolilo32@gmail.com>
2025-07-02 00:09:37 +0200

    Re-added config

---
+++ data/config.py
@@ -0,0 +1,56 @@
+TOKEN = ""
+CLIENT_ID = int("")
+
+JSON_FILE_PATH = "codici.json"
+VOTI_FILE = "voti.json"
+BACKGROUND_PATH = "images/backgroundacrom.png"
+FONT_PATH = "font/Minecraft.ttf"
+
+HOST = ""
+PORT = 3306
+USER = ""
+PASSWORD = ""
+DATABASE_SELECT = ""
+MAX_WARNS = 3
+
+DB_APPLY_PATH = "availability.db"
+
+ICONACROM = ""
+ICONBANNER = ""
+
+STORE_URL = ""
+VOTE_URL = ""
+INVITE_URL = ""
+
+ROLE_DEPX_ID = int("")
+MANAGE_WARN_ROLE_ID = int("")
+STAFF_ROLE_ID = int("")
+HEADSTAFF_ROLE_ID = int("")
+GESTORE_ACC_DISCORD_ID = int("")
+TICKET_ROLE_ID = int("")
+VERIFICATION_ROLE_ID = int("")
+NOT_VERIFIED_ROLE_ID = int("")
+
+CHANNEL_DEPEX_LOGS = int("")
+SUGGESTION_STAFF_LOGS_CHANNEL = int("")
+TRANSCRIPT_TICKET_CHANNEL = int("")
+WELCOME_CHANNEL_ID = int("")
+WELCOME_RULE_CHANNEL_ID = int("")
+
+CATEGORY_LIST = [
+    ("Supporto Generale", "Tickets Supporto Generale"),
+    ("Store", "Tickets Store"),
+    ("Report", "Tickets Report"),
+    ("Rollback", "Tickets Rollback"),
+    ("Contestazione Ban/Mute", "Tickets Contestazioni"),
+    #("Discord Account", "Tickets DsAccount"),
+    ("Altro", "Tickets Altro")
+]
+
+CATEGORY_DICT = dict(CATEGORY_LIST)
+
+GUILD_ID = int("")
+
+MINECRAFT_SERVER = ""
+
+TMZ = "Europe/Rome"
